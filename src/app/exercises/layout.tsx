35ab5d92648db4dea87190f38a689390f7064bf2
khoa vo <khoavo.private@gmail.com>
2025-08-20 09:12:47 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dir

--- src/app/exercises/layout.tsx
+++ src/app/exercises/layout.tsx
@@ -1,6 +1,4 @@
 import { exercisesConfig } from "@/config/exercises";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { SidebarNav } from "./_components/sidebar-nav";
 import React from "react";
 import { type Doc, type NavItem } from "@/types";
 import { MainNav } from "@/components/main-nav";
@@ -26,13 +24,7 @@ export default async function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <MainNav docs={allDocs} />
-      <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-8 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-12">
-        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-            <ScrollArea className="h-full py-6 pr-6 lg:py-8">
-              <SidebarNav items={navItems} />
-            </ScrollArea>
-        </aside>
-        
+      <div className="container flex-1">
         {children}
       </div>
     </div>
