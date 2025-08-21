cc53b84942fca841bde7cedf46b4579928a433fe
khoa vo <khoavo.private@gmail.com>
2025-08-21 06:19:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/app/blog-new/_components/author-info.tsx
+++ src/app/blog-new/_components/author-info.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Hand, MessageCircle } from "lucide-react";
+import { Hand, MessageCircle, Linkedin, Twitter, Facebook, Link as LinkIcon } from "lucide-react";
 import React from "react";
 
 type AuthorDetails = {
@@ -33,23 +33,23 @@ export function AuthorInfo({ author }: { author: AuthorDetails }) {
                 <Separator orientation="vertical" className="h-6" />
                 <div className="flex items-center gap-2">
                     <Button variant="ghost" size="icon" className="h-8 w-8">
-                        <Hand className="w-5 h-5" />
+                        <MessageCircle className="w-5 h-5" />
                     </Button>
                      <span>{author.comments}</span>
                 </div>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
                 <Button variant="ghost" size="icon" className="h-8 w-8">
-                    <Hand className="w-5 h-5" />
+                    <Linkedin className="w-5 h-5" />
                 </Button>
                 <Button variant="ghost" size="icon" className="h-8 w-8">
-                    <Hand className="w-5 h-5" />
+                    <Twitter className="w-5 h-5" />
                 </Button>
                  <Button variant="ghost" size="icon" className="h-8 w-8">
-                    <Hand className="w-5 h-5" />
+                    <Facebook className="w-5 h-5" />
                 </Button>
                 <Button variant="ghost" size="icon" className="h-8 w-8">
-                    <Hand className="w-5 h-5" />
+                    <LinkIcon className="w-5 h-5" />
                 </Button>
             </div>
         </div>
