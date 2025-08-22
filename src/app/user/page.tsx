cab51f844517df88bb53daad30b8cf48c56a0c7a
khoa vo <khoavo.private@gmail.com>
2025-08-22 12:17:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -325,7 +325,7 @@ export default function UserPage() {
               </TabsTrigger>
               <TabsTrigger value="manage-blog">Quản lý blog</TabsTrigger>
               <TabsTrigger value="saved-posts">Bài viết đã lưu</TabsTrigger>
-              <TabsTrigger value="support" className="flex items-center gap-1.5 font-semibold data-[state=inactive]:text-yellow-600 data-[state=active]:text-yellow-500">
+              <TabsTrigger value="support" className="flex items-center gap-1.5 font-semibold data-[state=inactive]:text-yellow-600 data-[state=active]:text-yellow-500 data-[state=inactive]:bg-yellow-500/10 data-[state=active]:bg-yellow-500/20">
                 <Star className="w-4 h-4" />
                 Trở thành người hỗ trợ
               </TabsTrigger>
